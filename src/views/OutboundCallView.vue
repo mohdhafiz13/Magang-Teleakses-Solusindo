d27c97e1c2fe5ed2becd89db040504874439ad90
kingwire666 <pertamaha963@gmail.com>
2026-09-21 10:51:05 +0700

    Complete outbound call data integration with Pinia

--- src/views/OutboundCallView.vue
+++ src/views/OutboundCallView.vue
@@ -1,72 +1,71 @@
 <template>
   <DefaultLayout>
-
     <div>
-      <h1 class="text-h5 font-weight-bold">
-        Outbound Call
-      </h1>
-      <p class="text-grey">
-        Outbound call monitoring page
-      </p>
+      <h1 class="text-h5 font-weight-bold">Outbound Call</h1>
 
-      <div class="mt-5">
-        <p>
-          Total Call :
-          {{ totalCall }}
-        </p>
-        <p>
-          Connected :
-          {{ connectedCall }}
-        </p>
-        <p>
-          Failed :
-          {{ failedCall }}
-        </p>
-        <p>
-          Pending :
-          {{ pendingCall }}
-        </p>
+      <p class="text-grey">Outbound call monitoring</p>
 
-        <hr class="my-5">
-        <h3>Outbound Call Data</h3>
-        <div
-          v-for="call in outboundCalls"
-          :key="call.id"
-          class="mt-3"
-        >
-          {{ call.customer }}
-          -
-          {{ call.agent }}
-          -
-          {{ call.status }}
+      <p>Total Call : {{ totalCall }}</p>
 
-        </div>
-      </div>
+      <p>Connected : {{ connectedCall }}</p>
 
+      <p>Failed : {{ failedCall }}</p>
+
+      <p>Pending : {{ pendingCall }}</p>
+
+      <hr />
+
+      <h3>Call List</h3>
+
+      <div 
+  v-for="call in outboundCalls" 
+  :key="call.id"
+>
+
+  <p>
+    Customer : {{ call.customer }}
+  </p>
+
+  <p>
+    Phone : {{ call.phone }}
+  </p>
+
+  <p>
+    Campaign : {{ call.campaign }}
+  </p>
+
+  <p>
+    Agent : {{ call.agent }}
+  </p>
+
+  <p>
+    Status : {{ call.status }}
+  </p>
+
+  <p>
+    Duration : {{ call.duration }}
+  </p>
+
+  <p>
+    Time : {{ call.callTime }}
+  </p>
+
+  <hr>
+
+</div>
     </div>
   </DefaultLayout>
 </template>
 
-
 <script setup>
-import { computed } from 'vue'
-import DefaultLayout from '../layouts/DefaultLayout.vue'
-import { useOutboundStore } from '../stores/outbound'
+import { computed } from "vue";
+import DefaultLayout from "../layouts/DefaultLayout.vue";
+import { useOutboundStore } from "../stores/outbound";
 
-const outboundStore = useOutboundStore()
-const outboundCalls = computed(() =>
-  outboundStore.outboundCalls
-)
-const totalCall = computed(() =>
-  outboundStore.totalCall
-)
-const connectedCall = computed(() =>
-  outboundStore.connectedCall
-)
-const failedCall = computed(() =>
-  outboundStore.failedCall
-)
-const pendingCall = computed(() =>
-  outboundStore.pendingCall
-)
+const outboundStore = useOutboundStore();
+const outboundCalls = computed(() => outboundStore.outboundCalls);
+const totalCall = computed(() => outboundStore.totalCall);
+const connectedCall = computed(() => outboundStore.connectedCall);
+const failedCall = computed(() => outboundStore.failedCall);
+const pendingCall = computed(() => outboundStore.pendingCall);
 </script>
